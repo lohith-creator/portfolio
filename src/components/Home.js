c1f070231a6e07ffdac298f499c17534ec5de323
lohith-creator <lohithkukkadapu@lewisu.edu>
2025-09-15 15:43:54 -0500

    Fixed Google Drive link in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -53,7 +53,7 @@ function Home() {
         {/* Action Buttons */}
         <div className="mt-8 flex space-x-4">
           <a
-  href="https://drive.google.com/file/d/1xA1p-iP3PljlFZhkJiYQor_fNBBqGLjZ/view?usp=sharing"
+  href="https://drive.google.com/file/d/1Dxw6lbrLj7VW0k0xdPTZI6-Lm0b_WjyO/view?usp=sharing"
   target="_blank"
   rel="noopener noreferrer"
   className="px-6 py-3 bg-red-500 border-2 border-red-500 text-white rounded-md hover:bg-red-500 transition"
